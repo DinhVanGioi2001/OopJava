f67933f6703bbfafc80c2222cb475c52520e05be
DinhVanGioi2001 <89461091+DinhVanGioi2001@users.noreply.github.com>
2021-11-11 18:33:29 +0700

    AC

--- IOFile/SoThuanNghichTrongFile.java
+++ IOFile/SoThuanNghichTrongFile.java
@@ -11,16 +11,30 @@ import java.util.TreeMap;
 
 public class SoThuanNghichTrongFile{
     
+    static int[] a= new int[1000001];    
+    static int[] b= new int[1000001];
+    
     public static boolean stn(int n){
         String s = "" + n; 
         int m = s.length();
-        if(n % 2 == 0 || m % 2 == 0 || m <= 1) return false;
-        for(int i = 0; i <= s.length()/2; i++){
-            if(s.charAt(i) == s.charAt(s.length() - i - 1) || (int)s.charAt(i) % 2 == 0){
+        if(m % 2 == 0 || m <= 1) return false;
+        for(int i = 0; i <= m/2; i++){
+            if(s.charAt(i) != s.charAt(m - i - 1) || (int)s.charAt(i) % 2 == 0){
                 return false;
             }
         }
         return true;
+//        String s = String.valueOf(n);
+//        String st = "";
+//        int m = s.length();
+//        int oke = 1;
+//        for(int i = m-1; i >= 0; i--){
+//            if((s.charAt(i) - 48) % 2 == 0){
+//                return false;
+//            }
+//            st += s.charAt(i);
+//        }
+//        return (m % 2 == 1 && s.equals(st));
     }
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DATA1.in"));
@@ -29,22 +43,19 @@ public class SoThuanNghichTrongFile{
         ois = new ObjectInputStream(new FileInputStream("DATA2.in"));
         ArrayList<Integer> list2 = (ArrayList<Integer>)ois.readObject();
         
-        TreeMap<Integer, Integer> tm = new TreeMap<>();
-        int n = 1;
+        int n = 0;
         for(Integer i: list1){
-            if(list2.contains(i) && i % 2 == 1){
-                if(tm.containsKey(i)){
-                    tm.put(i, tm.get(i));
-                }else{
-                    if(n <= 10 && stn(i)){
-                        tm.put(i, 1);
-                        n++;
-                    }
-                }
-            }
+            a[i]++;
         }
-        for(Integer i: tm.keySet()){
-            System.out.println(i + " " + tm.get(i));
+        for(Integer i: list2){
+            b[i]++;
+        }
+        for(int i = 11; i <= 1000000; i++){
+            if(a[i] > 0 && b[i] > 0 && stn(i)){
+                System.out.println(i + " " + (a[i] + b[i]));
+                n++;
+            }
+            if(n== 10) break;
         }
     }
 }
