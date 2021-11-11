b05bd7a4ea6276ec19b089474b29d5ca44247844
DinhVanGioi2001 <89461091+DinhVanGioi2001@users.noreply.github.com>
2021-11-11 17:36:26 +0700

    IR

--- IOFile/SoThuanNghichTrongFile.java
+++ IOFile/SoThuanNghichTrongFile.java
@@ -23,20 +23,23 @@ public class SoThuanNghichTrongFile{
         return true;
     }
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-        ObjectInputStream din = new ObjectInputStream(new FileInputStream("DATA1.in"));
-        ArrayList<Integer> list1 = (ArrayList<Integer>)din.readObject();
-        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DATA1.in"));
+        ArrayList<Integer> list1 = (ArrayList<Integer>)ois.readObject();
         
-        din = new ObjectInputStream(new FileInputStream("DATA2.in"));
-        ArrayList<Integer> list2 = (ArrayList<Integer>)din.readObject();
+        ois = new ObjectInputStream(new FileInputStream("DATA2.in"));
+        ArrayList<Integer> list2 = (ArrayList<Integer>)ois.readObject();
+        
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
         int n = 1;
         for(Integer i: list1){
-            if(tm.containsKey(i)){
-                tm.put(i, tm.get(i) + 1);
-            }else{
-                if(n <= 10 && list2.contains(i) && stn(n)){
-                    tm.put(i, 1);
-                    n++;
+            if(list2.contains(i) && i % 2 == 1){
+                if(tm.containsKey(i)){
+                    tm.put(i, tm.get(i));
+                }else{
+                    if(n <= 10 && stn(i)){
+                        tm.put(i, 1);
+                        n++;
+                    }
                 }
             }
         }
